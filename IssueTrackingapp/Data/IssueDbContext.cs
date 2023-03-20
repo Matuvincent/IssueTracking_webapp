28f2ee9541a2a060ce0665575bc531de7fc6b2db
Matuvincent <88109080+Matuvincent@users.noreply.github.com>
2023-03-20 01:30:46 -0700

    new

---
+++ IssueTrackingapp/Data/IssueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IssueTrackingapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTrackingapp.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions options) :base(options)
+        {
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
---
+++ IssueTrackingapp/Pages/Issues/New.cshtml.cs
@@ -0,0 +1,25 @@
+using IssueTrackingapp.Data;
+using IssueTrackingapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IssueTrackingapp.Pages.Issues
+{
+    public class NewModel : PageModel
+    {
+        private readonly IssueDbContext _context;
+
+        public NewModel(IssueDbContext context) => _context = context;
+    public async Task<IActionResult> OnPost()
+        { 
+            if(!ModelState.IsValid) return Page();
+            Issue.Created = DateTime.Now;
+            await _context.Issues.AddAsync(new Issue { });
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("../Index");
+        }
+        [BindProperty]
+        public Issue Issue { get; set; }
+    }
+}
